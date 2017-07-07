6b14581580d064d5548d42242213f77c9f56b291
Évelyne Lachance <eslachance@gmail.com>
2017-07-07 00:35:08 -0400

    Cleaned up clean function to be cleaner
    Await works for non-promise returns, there's no need to check if it's a promise. TIL.

--- modules/functions.js
+++ modules/functions.js
@@ -44,16 +44,12 @@ module.exports = (client) => {
   };
   
   client.clean = async (text) => {
-    if (text && text.constructor.name == 'Promise')
-      text = await text;
-    if (typeof evaled !== 'string')
-      text = require('util').inspect(text, {depth: 0});
-    
-    text = text
-      .replace(/`/g, "`" + String.fromCharCode(8203))
-      .replace(/@/g, "@" + String.fromCharCode(8203))
-      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
-    
+    text = await text;
+    if (typeof text !== 'string')
+      text = require('util').inspect(text, {depth: 0})
+        .replace(/`/g, "`" + String.fromCharCode(8203))
+        .replace(/@/g, "@" + String.fromCharCode(8203))
+        .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
     return text;
   };
 
